983a47627d7ee2f8959263f7d0cb530ef02a1277
Wesley <wesathome@outlook.com>
2019-01-12 12:05:33 -0500

    commit two: electric boogaloo. added header file and readme.

---
+++ README.md
@@ -0,0 +1,2 @@
+# RoboRio2019
+5561's C++ code for our 2019 RoboRio.
---
+++ src/main/include/Robot.h
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <string>
+
+#include <frc/IterativeRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+
+#include <frc/WPILib.h>
+#include "ctre/Phoenix.h"
+
+using namespace frc;
+
+
+class Robot : public frc::IterativeRobot {
+ public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
+
+ private:
+ 
+  TalonSRX * _talon0 = new TalonSRX(1);
+};
